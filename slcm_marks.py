d29e6d87509a08ddd2c8575f517616f8518a406e
ArjunPraveen <arjun.praveen@learner.manipal.edu>
2020-06-21 01:21:54 +0400

    Exceptions and main function added

--- slcm_marks.py
+++ slcm_marks.py
@@ -23,30 +23,6 @@ def configure():
         return driver
     except SessionNotCreatedException:
         print("Driver forcefully shut!")  
-
-
-options = webdriver.ChromeOptions()
-
-options.add_argument('--log-level=3')
-#options.add_argument('headless')
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome("C:/Users/arjun/Downloads/chromedriver_win32/chromedriver.exe", options=options)
-delay = 15
-
-try:
-    driver.get(login_url)
-    driver.implicitly_wait(5)
-    name = driver.find_element_by_name('txtUserid')
-    name.send_keys(Username)
-    password = driver.find_element_by_name('txtpassword')
-    password.send_keys(Password)
-    driver.find_element_by_name('btnLogin').click()
-    print("Succesfully logged in")
-    academicsPage = EC.presence_of_element_located((By.ID, 'rtpchkMenu_lnkbtn2_1'))
-    
-    myElem = WebDriverWait(driver, delay).until(academicsPage)
-    myElem.click()
-    
     
 def login():
     try:
